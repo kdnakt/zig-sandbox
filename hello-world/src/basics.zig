f5eb05e0c45286b74b98262d9fed7f0b029c8501
kdnakt <a.kid.1985@gmail.com>
2022-07-17 11:53:35 +0900

    assign unsinged 32bit variable and print

--- hello-world/src/basics.zig
+++ hello-world/src/basics.zig
@@ -4,4 +4,7 @@ pub fn main() void {
     const constant: i32 = 5;
     std.debug.print("{d}\n", .{constant});
     // constant = 6; // cannot assign
+
+    var variable: u32 = 5000;
+    std.debug.print("{d}\n", .{variable});
 }
