a528f74bee0d1f9481bb04f49887106e3bf63247
kdnakt <a.kid.1985@gmail.com>
2022-07-18 11:03:51 +0900

    test if expression

--- hello-world/src/basics.zig
+++ hello-world/src/basics.zig
@@ -44,3 +44,10 @@ test "if statement" {
     }
     try expect(x == 1);
 }
+
+test "if statement expression" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    try expect(x == 1);
+}
