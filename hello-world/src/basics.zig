13b9f23f72e4ee99520e0e65dc4d71cf89f078ff
kdnakt <a.kid.1985@gmail.com>
2022-08-07 00:39:11 +0900

    create a slice from an array

--- hello-world/src/basics.zig
+++ hello-world/src/basics.zig
@@ -261,3 +261,15 @@ test "pointers" {
 //     _ = y;
 // }
 
+fn total(values: []const u8) usize {
+    var sum: usize = 0;
+    for (values) |v| sum += v;
+    return sum;
+}
+
+test "slices" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(total(slice) == 6);
+}
+
